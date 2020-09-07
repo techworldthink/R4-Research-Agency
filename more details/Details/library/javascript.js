991f6f5b1fba587c55e28cdedaa36a99e8c50d77
hat_lover <jobin25051999@gmail.com>
2020-09-07 17:53:10 +0530

    nav added to linux page

---
+++ more details/Details/library/javascript.js
@@ -0,0 +1,35 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+ $('body').on('click', function(){
+    if( parseInt( $('#mySidenav').css('width') ) > 0 ){
+      closeNav();
+    }
+  });
+
+
+  //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
